ccce39cfeaaeb0123e8f5a7c44f5e744cc389ea6
Lucas Czech <luc.czech@gmail.com>
2026-04-05 21:21:09 -0300

    Add test setup

---
+++ test/src/common.hpp
@@ -0,0 +1,162 @@
+#ifndef SPEAR_TEST_COMMON_H_
+#define SPEAR_TEST_COMMON_H_
+
+/*
+    SPEAR - Sorting Petabytes of Environmental and Ancient Reads
+    Copyright (C) 2026 Lucas Czech
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact:
+    Lucas Czech <lucas.czech@sund.ku.dk>
+    University of Copenhagen, Globe Institute, Section for GeoGenetics
+    Oster Voldgade 5-7, 1350 Copenhagen K, Denmark
+*/
+
+/**
+ * @brief
+ *
+ * @file
+ * @ingroup test
+ */
+
+// =================================================================================================
+//     Common Headers
+// =================================================================================================
+
+#include "genesis/util/core/logging.hpp"
+
+// =================================================================================================
+//     Compiler Details
+// =================================================================================================
+
+// Disable warnings for GCC, also works in Clang, see http://stackoverflow.com/a/13492589/4184258
+// Furthermore, while we are at it, also disable the warning produced by the pragma
+// for disabling warnings in MSVC...
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
+// Disable warnings for MSVC, see http://stackoverflow.com/a/2106590/4184258
+#pragma warning (push, 0)
+
+// Include the GTest header, which produces ugly warnings with our compile flags.
+#include <gtest/gtest.h>
+
+// Re-enable warnings
+#pragma warning (pop)
+#pragma GCC diagnostic pop
+
+// =================================================================================================
+//     Useful Macros
+// =================================================================================================
+
+/**
+ * @brief Macro to be used in test bodies that depend on the data directory.
+ * If it was not found on program startup, the data files cannot be used, thus
+ * tests using them need to be skipped.
+ */
+#define NEEDS_TEST_DATA                                    \
+    if(environment->data_dir == "") {                      \
+        if (environment->fail_on_missing_data_dir) {       \
+            FAIL() << "No test data directory found.";     \
+        }                                                  \
+        return;                                            \
+    };
+
+#define EXPECT_ITERABLE_DOUBLE_EQ( TYPE, ref, target) \
+{ \
+    const TYPE& _ref(ref); \
+    const TYPE& _target(target); \
+    TYPE::const_iterator tarIter   = _target.begin(); \
+    TYPE::const_iterator refIter = _ref.begin(); \
+    unsigned int i = 0; \
+    while(refIter != _ref.end()) { \
+        if ( tarIter == _target.end() ) { \
+            ADD_FAILURE() << #target \
+                " has a smaller length than " #ref ; \
+            break; \
+        } \
+        EXPECT_DOUBLE_EQ(* refIter, * tarIter) \
+            << "Vectors " #ref  " (refIter) " \
+               "and " #target " (tarIter) " \
+               "differ at index " << i; \
+        ++refIter; ++tarIter; ++i; \
+    } \
+    EXPECT_TRUE( tarIter == _target.end() ) \
+        << #ref " has a smaller length than " \
+           #target ; \
+}
+
+#define EXPECT_ITERABLE_DOUBLE_NEAR( TYPE, ref, target, delta) \
+{ \
+    const TYPE& _ref(ref); \
+    const TYPE& _target(target); \
+    TYPE::const_iterator tarIter   = _target.begin(); \
+    TYPE::const_iterator refIter = _ref.begin(); \
+    unsigned int i = 0; \
+    while(refIter != _ref.end()) { \
+        if ( tarIter == _target.end() ) { \
+            ADD_FAILURE() << #target \
+                " has a smaller length than " #ref ; \
+            break; \
+        } \
+        EXPECT_NEAR(* refIter, * tarIter, delta) \
+            << "Vectors " #ref  " (refIter) " \
+               "and " #target " (tarIter) " \
+               "differ at index " << i; \
+        ++refIter; ++tarIter; ++i; \
+    } \
+    EXPECT_TRUE( tarIter == _target.end() ) \
+        << #ref " has a smaller length than " \
+           #target ; \
+}
+
+// =================================================================================================
+//     Test Environment
+// =================================================================================================
+
+/**
+ * @Brief Environment class for testing spear.
+ */
+class SpearTestEnvironment : public ::testing::Environment {
+public:
+
+    // -------------------------------------------------------------------------
+    //     Gtest Functions
+    // -------------------------------------------------------------------------
+
+    virtual ~SpearTestEnvironment() {}
+
+    // Override this to define how to set up the environment.
+    virtual void SetUp() {}
+
+    // Override this to define how to tear down the environment.
+    virtual void TearDown() {}
+
+    // -------------------------------------------------------------------------
+    //     Global Settings
+    // -------------------------------------------------------------------------
+
+    std::string data_dir;
+
+    bool fail_on_missing_data_dir;
+};
+
+/**
+ * @brief Instance of the SpearTestEnvironment, which is set in `main()`.
+ */
+extern SpearTestEnvironment* environment;
+
+#endif // include guard
---
+++ test/src/main.cpp
@@ -0,0 +1,121 @@
+/*
+    SPEAR - Sorting Petabytes of Environmental and Ancient Reads
+    Copyright (C) 2026 Lucas Czech
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact:
+    Lucas Czech <lucas.czech@sund.ku.dk>
+    University of Copenhagen, Globe Institute, Section for GeoGenetics
+    Oster Voldgade 5-7, 1350 Copenhagen K, Denmark
+*/
+
+/**
+ * @brief Main function for testing spear using Gtest.
+ *
+ * @file
+ * @ingroup test
+ */
+
+#include <gtest/gtest.h>
+
+#include <csignal>
+#include <iostream>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "src/common.hpp"
+#include "genesis/util/core/options.hpp"
+
+SpearTestEnvironment* environment;
+
+void spear_test_sighandler( int signum )
+{
+    // Without special tricks, we might not get a core dump when a SIGSEGV occurs.
+    // So we overwrite the default handler to achieve that.
+    // See https://stackoverflow.com/a/19308537
+    LOG_ERR << "Segmentation fault (SIGSEGV)";
+    std::signal( signum, SIG_DFL );
+    ::kill( ::getpid(), signum );
+}
+
+/**
+ * @brief Initializes and starts the test run.
+ *
+ * The main sets the environment for testing and then runs the tests.
+ *
+ * @return      Zero if all tests were successfull, non-zero otherwise.
+ */
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    environment = new SpearTestEnvironment();
+
+    // Set a custom signal handler, see there for details.
+    // Sometimes, this however does the opposite, and does not dump a core when segfauling...
+    // So deactivating this for now again.
+    // signal( SIGSEGV, spear_test_sighandler );
+
+    // Set data dir using the program path.
+    std::string call = argv[0];
+    std::size_t found = call.find_last_of("/\\");
+    if (found != std::string::npos) {
+
+        // Ugly hardcoded path depending on the build structe of the program.
+        // For now, this is good enoguh, but maybe we should find a more portable solution.
+        environment->data_dir = call.substr(0,found) + "/../../test/data/";
+    }
+
+    // If user provided one via command line, overwrite data dir.
+    std::string data_dir_prefix = "--test_data_dir=";
+    for (int i = 1; i < argc; ++i) {
+        std::string argument = argv[i];
+        if(argument.substr(0, data_dir_prefix.size()) == data_dir_prefix) {
+            environment->data_dir = argument.substr(data_dir_prefix.size());
+        }
+    }
+
+    // Return true iff the directory exists.
+    // This is copied from the genesis library `./genesis/util/core/fs.hpp`
+    // because we do not want to rely on something that we are about to test...
+    auto dir_exists = [] (const std::string& dir )
+    {
+        struct stat info;
+        if (stat (dir.c_str(), &info) != 0) {
+            return false;
+        }
+        return static_cast<bool>( info.st_mode & S_IFDIR );
+    };
+
+    // Actually, use the data dir only if it exists.
+    if (!dir_exists(environment->data_dir)) {
+        environment->data_dir = "";
+    }
+
+    // We want to fail if we didn't find the test data.
+    environment->fail_on_missing_data_dir = true;
+
+    // We want to see Logging information while testing.
+    genesis::util::core::Logging::log_to_stdout();
+    genesis::util::core::Logging::max_level(genesis::util::core::Logging::kDebug4);
+
+    // Start the global thread pool, once here.
+    // Needed by some of the tests.
+    genesis::util::core::Options::get().init_global_thread_pool( 2 );
+
+    ::testing::AddGlobalTestEnvironment(environment);
+    return RUN_ALL_TESTS();
+}
